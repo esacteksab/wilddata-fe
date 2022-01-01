59ce75c227f6b45c7f2cce88a57274ea627ab0d8
Barry Morrison <b@rrymorrison.com>
2022-01-01 16:19:22 -0600

    new shiny

--- .svelte-kit/dev/generated/root.svelte
+++ .svelte-kit/dev/generated/root.svelte
@@ -33,15 +33,19 @@
 	});
 </script>
 
-<svelte:component this={components[0]} {...(props_0 || {})}>
-	{#if components[1]}
-		<svelte:component this={components[1]} {...(props_1 || {})}>
-			{#if components[2]}
+{#if components[1]}
+	<svelte:component this={components[0]} {...(props_0 || {})}>				
+		{#if components[2]}
+			<svelte:component this={components[1]} {...(props_1 || {})}>				
 				<svelte:component this={components[2]} {...(props_2 || {})}/>
-			{/if}
-		</svelte:component>
-	{/if}
-</svelte:component>
+			</svelte:component>
+		{:else}
+			<svelte:component this={components[1]} {...(props_1 || {})} />
+		{/if}
+	</svelte:component>
+{:else}
+	<svelte:component this={components[0]} {...(props_0 || {})} />
+{/if}
 
 {#if mounted}
 	<div id="svelte-announcer" aria-live="assertive" aria-atomic="true">
