e5f7ca882f5a3b811c36effa17bd65fbbed4c9e9
Barry Morrison <689591+esacteksab@users.noreply.github.com>
2022-04-10 16:10:36 -0500

    chore: replacing Name with DisplayName due to BE change (#178)

--- src/lib/Org.svelte
+++ src/lib/Org.svelte
@@ -6,7 +6,7 @@
 
 <foo class="py-4 text-lg">
 
-        <p><a class="underline text-orange-600" href="o/{org.Name}">Name: {org.Name}</a></p>
+        <p><a class="underline text-orange-600" href="o/{org.Name}">Name: {org.DisplayName}</a></p>
         <p>EMail: {org.EMail}</p>
 <br/>
     <hr />
