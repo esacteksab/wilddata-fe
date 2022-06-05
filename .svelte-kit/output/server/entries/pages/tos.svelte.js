59231d433dd9618853715471dfdb9e79655814df
Barry Morrison <689591+esacteksab@users.noreply.github.com>
2022-06-05 15:39:36 -0500

    chore: more styling and some cleanup in the repo (#310)

--- .svelte-kit/output/server/entries/pages/tos.svelte.js
+++
@@ -1,7 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/index-fa0ff56f.js";
-const Tos = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<h1>Terms of Service</h1>
-
-A really large amount of shit you&#39;re not going to read.`;
-});
-export { Tos as default };
