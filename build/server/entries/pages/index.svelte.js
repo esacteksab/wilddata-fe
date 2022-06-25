e63604505fcb5e1512026e4d1cd7c8146b80562c
Barry Morrison <689591+esacteksab@users.noreply.github.com>
2022-06-25 10:52:37 -0500

    chore: removing build dir from source

--- build/server/entries/pages/index.svelte.js
+++
@@ -1,5 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/index-fa0ff56f.js";
-const Routes = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<main><div class="${"container flex flex-wrap mx-auto"}"><div class="${"flex flex-row"}"></div></div></main>`;
-});
-export { Routes as default };
--- build/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-import * as module from "../entries/fallbacks/error.svelte.js";
-
-export { module };
-export const index = 1;
-export const entry = "error.svelte-6f0ac394.js";
-export const js = ["error.svelte-6f0ac394.js", "chunks/index-9dae82f1.js"];
-export const css = [];
